156284a262879f841a9dbe70f5ac3ec11cd28a51
Brian Roy <brian.roy@Brians-Mac-mini.local>
2025-09-09 11:47:49 -0600

    updating the look and feel of the timeline

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -4,7 +4,10 @@ const EventCard = ({ event, onSelectMedia }) => {
     const eventDate = new Date(event.event_ts * 1000);
 
     return (
-        <div className="event-card">
+        <div className="event-card" onClick={() => onSelectMedia(event)}>
+            <div className="event-card-thumbnail">
+                {/* Placeholder for thumbnail */}
+            </div>
             <div className="event-card-body">
                 <div className="event-card-header">
                     <span className="camera-name">{event.camera_name}</span>
@@ -13,9 +16,9 @@ const EventCard = ({ event, onSelectMedia }) => {
                 <div className="event-card-details">
                     <span className="event-type">{event.event_type}</span>
                 </div>
-                <button className="view-button" onClick={() => onSelectMedia(event)}>
-                    Play
-                </button>
+            </div>
+            <div className="event-card-play-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="24px" height="24px"><path d="M8 5v14l11-7z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
             </div>
         </div>
     );
